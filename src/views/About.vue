adf29f63e2dc6ab513305acff771763127496b60
Mohamed <nrookman_27@yahoo.com>
2022-03-20 19:48:00 +0200

    Add Bootstrap

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p class="h3">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam magni
+      dicta vitae aut recusandae fuga expedita consequatur, debitis cupiditate
+      ab cumque voluptatibus accusantium molestiae obcaecati, eaque quo
+      blanditiis minus? Quo.
+    </p>
   </div>
 </template>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="contact">
     <h1>This is an contact page</h1>
+    <p class="h3">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+      facere sint error, beatae tempore exercitationem magni, rerum libero
+      architecto eaque vel neque deleniti velit cupiditate similique quibusdam
+      quam quaerat voluptates!
+    </p>
   </div>
 </template>
